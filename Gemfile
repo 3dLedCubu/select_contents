0e2af11224c9a168d28ca60700954e9bdebae18b
fxat00768 <hiroshi.mikuriya@fxat.co.jp>
2018-06-13 10:21:51 +0900

    add skelton

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+ruby '= 2.5.0'
+
+gem 'coffee-script'
+gem 'haml'
+gem 'sass'
+gem 'sinatra'
+gem 'sinatra-contrib'
---
+++ app.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+require 'coffee-script'
+require 'socket'
+
+##
+# Server program
+class App < Sinatra::Base
+  register Sinatra::Reloader
+  enable :sessions
+  set :bind, '0.0.0.0' # 外部アクセス可
+  Tilt::CoffeeScriptTemplate.default_bare = true # coffeescriptの即時関数を外す
+
+  get '/' do
+    haml :index, layout: :layout, locals: { title: 'select contents' }
+  end
+end
