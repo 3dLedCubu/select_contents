72e224e6d239f4d213ae7bc66cad594c2daeefa6
加峯茉以子 <kabumaiko@chabukan-2.local>
2018-07-17 22:21:54 +0900

    デモ・消灯・背景の画像追加、消灯を接続しないように

--- app.rb
+++ app.rb
@@ -9,28 +9,30 @@ require 'net/http'
 require 'uri'
 
 $large_contents = [
-  { id: 'lego', name: 'ブロック', port: 5000, selected: false, unselect_img: 'assets/block1.png', select_img: 'assets/block2.png' },
-  { id: 'paint', name: 'おえかき', port: 5302, selected: false, unselect_img: 'assets/paint1.png', select_img: 'assets/paint2.png' },
-  { id: 'camera', name: 'カメラ', port: 5402, selected: false, unselect_img: 'assets/camera1.png', select_img: 'assets/camera2.png' }
+  { id: 'lego', name: 'ブロック', port: 5101, sound_port: 5000, selected: false, unselect_img: 'assets/block1.png', select_img: 'assets/block2.png' },
+  { id: 'paint', name: 'おえかき', port: 5301, sound_port: 5302, selected: false, unselect_img: 'assets/paint1.png', select_img: 'assets/paint2.png' },
+  { id: 'camera', name: 'カメラ', port: 5401, sound_port: 5402, selected: false, unselect_img: 'assets/camera1.png', select_img: 'assets/camera2.png' }
 ]
 # $large_contents = [
-#   { id: 'lego', name: 'ブロック', port: 5000, selected: false },
-#   { id: 'paint', name: 'おえかき', port: 5000, selected: false },
-#   { id: 'camera', name: 'カメラ', port: 5000, selected: false }
+#   { id: 'lego', name: 'ブロック', sound_port: 5000, selected: false, unselect_img: 'assets/block1.png', select_img: 'assets/block2.png'  },
+#   { id: 'paint', name: 'おえかき', sound_port: 5000, selected: false, unselect_img: 'assets/paint1.png', select_img: 'assets/paint2.png'  },
+#   { id: 'camera', name: 'カメラ', sound_port: 5000, selected: false, unselect_img: 'assets/camera1.png', select_img: 'assets/camera2.png' }
 # ]
 
 $small_contents = [
-  { id: 'screen_saver', name: 'デモ', port: 5201, selected: false, unselect_img: 'assets/demo1.png', select_img: 'assets/demo2.png'  }
+  { id: 'screen_saver', name: 'デモ', port: 5201, selected: false, unselect_img: 'assets/Kit_btn_Demo_Off.png', select_img: 'assets/Kit_btn_Demo_On.png'  }
 ]
 
-$lower_right_contents = [
-  { id: 'light_off', name: '消灯', port: 5501, selected: false, unselect_img: 'assets/off1.png', select_img: 'assets/off2.png'  }
+$light_off_contents = [
+  { id: 'light_off', name: '消灯', selected: false, unselect_img: 'assets/Kit_btn_LED_Off.png', select_img: 'assets/Kit_btn_LED_On.png'  }
 ]
 
-$contents = $large_contents + $small_contents + $lower_right_contents
 
+$contents = $large_contents + $small_contents + $light_off_contents
+$contents_except_light_off = $large_contents + $small_contents
 
 ##
+
 # Server program
 class App < Sinatra::Base
   register Sinatra::Reloader
@@ -51,25 +53,25 @@ class App < Sinatra::Base
 
   def initialize
     super
-    $contents.each { |c| Thread.new { loop { flow(c) } } }
+    $contents_except_light_off.each { |c| Thread.new { loop { flow(c) } } }
   end
 
   get '/' do
-    #@contents = $contents
     @large_contents = $large_contents
     @small_contents = $small_contents
-    @lower_right_contents = $lower_right_contents
+    @light_off_contents = $light_off_contents
     haml :index, locals: { title: '3D LED' }
   end
+
 
   post '/select' do
     id = params['id']
 
     # $large_contents.each do |c|
     #   if (c[:id] == id)
-    #     select_volume(c[:port],100)
+    #     select_volume(c[:sound_port],100)
     #   else
-    #     select_volume(c[:port],0)
+    #     select_volume(c[:sound_port],0)
     #   end
     # end
 
@@ -84,9 +86,9 @@ class App < Sinatra::Base
     volume = params['volume']
   end
 
-  def select_volume(port, volume)
-    url = 'http://192.168.0.20:'+port.to_s+'/api/audio'
-    #url = 'http://localhost:'+port.to_s+'/api/audio'
+  def select_volume(sound_port, volume)
+    url = 'http://192.168.0.20:'+sound_port.to_s+'/api/audio'
+    #url = 'http://localhost:'+sound_port.to_s+'/api/audio'
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = false
